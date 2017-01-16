b3b8b07365840a46fa01dfb263afa2f9cbffd8c6
Jerome Bergmann <jerome.bergmann@rwth-aachen.de>
2017-01-16 22:17:39 +0100

    Arpeggio Parser example tryout

--- src/omniglot/parser.py
+++ src/omniglot/parser.py
@@ -1,11 +1,20 @@
 from arpeggio import Optional, ZeroOrMore, OneOrMore, EOF
 from arpeggio import RegExMatch as _
-
-def number():     return _(r'\d*\.\d*|\d+')
-def factor():     return Optional(["+","-"]), [number, ("(", expression, ")")]
-def term():       return factor, ZeroOrMore(["*","/"], factor)
-def expression(): return term, ZeroOrMore(["+", "-"], term)
-def calc():       return OneOrMore(expression), EOF
-
 from arpeggio import ParserPython
+
+def number():
+    return _(r'\d*\.\d*|\d+')
+
+def factor():
+    return Optional(["+","-"]), [number, ("(", expression, ")")]
+
+def term():
+    return factor, ZeroOrMore(["*","/"], factor)
+
+def expression():
+    return term, ZeroOrMore(["+", "-"], term)
+
+def calc():
+    return OneOrMore(expression), EOF
+
 parser = ParserPython(calc)

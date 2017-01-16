f42f7a779fb8a9aa9b93799292467f2d92e8322d
Jerome Bergmann <jerome.bergmann@rwth-aachen.de>
2017-01-16 22:20:07 +0100

    PEP8 reformatting

--- src/omniglot/parser.py
+++ src/omniglot/parser.py
@@ -2,17 +2,22 @@ from arpeggio import Optional, ZeroOrMore, OneOrMore, EOF
 from arpeggio import RegExMatch as _
 from arpeggio import ParserPython
 
+
 def number():
     return _(r'\d*\.\d*|\d+')
 
+
 def factor():
-    return Optional(["+","-"]), [number, ("(", expression, ")")]
+    return Optional(["+", "-"]), [number, ("(", expression, ")")]
+
 
 def term():
-    return factor, ZeroOrMore(["*","/"], factor)
+    return factor, ZeroOrMore(["*", "/"], factor)
+
 
 def expression():
     return term, ZeroOrMore(["+", "-"], term)
+
 
 def calc():
     return OneOrMore(expression), EOF
